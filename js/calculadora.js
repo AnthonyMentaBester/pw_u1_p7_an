322e85bbd93db08ae23e7df0d2fe3d5f8b6e71ff
AnthonyMentaBester <ajnarvaezr@uce.edu.ec>
2024-11-22 22:46:12 +0000

    TAREA CALCULADORA

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,38 +1,68 @@
 var op = "";
 var numero1 = "";
 var numero2 = "";
-var operador = "";
+var operadoracion = "";
 var respuesta = "";
 
 
 function setearvalor(valor){
-    let valorActual = document.getElementById('id-display').innerText;
+    let valorActual = document.getElementById('id-display');
     console.log(valorActual);
     document.getElementById('id-display').innerText=valor;
 }
 
-function setearvalorConcatenado(valor){
-    let valorActual = document.getElementById('id-display').innerText;
-    console.log(valorActual);
-    document.getElementById('id-display').innerText=valorActual + valor;
+function setearvalorConcatenado(valor) {
+    let valorActual = document.getElementById("id-display").innerText;
+    if (valorActual === `${numero1}${op}`) {
+        document.getElementById('id-display').innerText += valor;
+    } else if (valorActual === "0") {
+        document.getElementById('id-display').innerText = valor;
+    } else {
+        document.getElementById('id-display').innerText += valor;
+    }
 }
+
+
 
 function operacion(valor) {
-    const display = document.getElementById("id-display");
+    const valorActual = document.getElementById("id-display");
+
     if (numero1 === "") {
-        numero1 = display.innerText;
+        numero1 = valorActual.innerText;
     } else if (respuesta !== "") {
         numero1 = respuesta.toString();
+        respuesta = ""; 
     }
     op = valor;
-    display.innerText = "0";
+    if (!valorActual.innerText.includes(op)) {
+        valorActual.innerText += valor;
+    }
 }
 
+
+function vaciar() {
+    const valorActual = document.getElementById("id-display");
+    valorActual.innerText = "0";
+    numero1 = "";
+    numero2 = "";
+    op = "";
+    respuesta = "";
+}
+
+function borrar() {
+    const valorActual = document.getElementById("id-display");
+
+    if (valorActual.innerText.length > 1) {
+        valorActual.innerText = valorActual.innerText.slice(0, -1);
+    } else {
+        valorActual.innerText = "0";
+    }
+}
 
 function calcular() {
 
-    const display = document.getElementById("id-display");
-    numero2 = display.innerText;
+    const valorActual = document.getElementById("id-display");
+    numero2 = valorActual.innerText;
 
     const num1 = parseFloat(numero1);
     const num2 = parseFloat(numero2);
@@ -55,10 +85,10 @@ function calcular() {
             }
             break;
         default:
-            respuesta = "ERROR";
+            respuesta = valor;
     }
 
-    display.innerText = `${numero1} ${op} ${numero2} = ${respuesta}`;
+    valorActual.innerText = `${numero1}${op}${numero1} = ${respuesta}`;
     numero1 = "";
     numero2 = "";
     op = "";
